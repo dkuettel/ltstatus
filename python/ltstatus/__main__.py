720749fa83b56e1a1fc0c4700de3a4cbf4c9e604
Daniel Kuettel <dkuettel@gmail.com>
2021-02-28 21:16:16 +0100

    run script and __main__ module for testing

---
+++ python/ltstatus/__main__.py
@@ -0,0 +1,36 @@
+from ltstatus import RateLimitedMonitors, RegularGroupMonitor, StdoutStatus, monitors
+
+monitor = RateLimitedMonitors(
+    monitors=[
+        monitors.nvidia.Monitor(),
+        monitors.datetime.Monitor(),
+        monitors.redshift.Monitor(),
+        monitors.dropbox.Monitor(),
+        RegularGroupMonitor(
+            monitors=[
+                monitors.cpu.Monitor(),
+                monitors.diskspace_alerts.Monitor(),
+                monitors.bluetooth.Monitor(),
+                monitors.spotify.Monitor(),
+                monitors.sound.Monitor(),
+            ],
+        ),
+    ],
+)
+
+status = StdoutStatus(
+    monitor=monitor,
+    order=[
+        "spotify",
+        "redshift",
+        "bluetooth",
+        "sound",
+        "dropbox",
+        "diskspace-alerts",
+        "cpu",
+        "nvidia",
+        "datetime",
+    ],
+)
+
+status.run()
