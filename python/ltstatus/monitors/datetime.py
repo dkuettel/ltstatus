4d8537b546123e115adb3a1fa30f952246c3ad28
Daniel Kuettel <dkuettel@gmail.com>
2021-03-11 18:17:43 +0100

    expose datetime formatting

--- python/ltstatus/monitors/datetime.py
+++ python/ltstatus/monitors/datetime.py
@@ -1,17 +1,18 @@
 import time
 from dataclasses import dataclass
 
-from ltstatus import ThreadedMonitor, State
+from ltstatus import State, ThreadedMonitor
 
 
 @dataclass
 class Monitor(ThreadedMonitor):
     name: str = "datetime"
+    format: str = r"%Y-%m-%d %a %H:%M"
 
     def run(self):
         while not self.exit.is_set():
             t = time.localtime()
-            content = time.strftime(r"%Y-%m-%d %a %H:%M", t)
+            content = time.strftime(self.format, t)
             self.queue.put(State.from_one(self.name, content))
             t = time.localtime()
             self.exit.wait(60 - t.tm_sec + 1)
