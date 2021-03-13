617da66a05d18fbef7c6cb2e75c0134e74e21586
Daniel Kuettel <dkuettel@gmail.com>
2021-03-13 16:35:34 +0100

    fix sound parsing in case of mono devices (like often a headset)

--- python/ltstatus/monitors/sound.py
+++ python/ltstatus/monitors/sound.py
@@ -5,13 +5,10 @@ from typing import Dict
 from ltstatus import CallbackMonitor, State
 from ltstatus.tools import ffield, run_cmd
 
-re_sound = re.compile(
-    r"""\* index: \d+
-(.*?\n)*?\tvolume: .* (?P<volume>\d+%) .*
-(.*?\n)*?\tmuted: (?P<muted>no|yes)
-(.*?\n)*?\t\tdevice.description = \"(?P<name>.*)\"
-"""
-)
+re_default_sink = re.compile(r" +\* index: \d+\n(?P<default_sink>(\t.+\n)+)")
+re_volume = re.compile(r"\tvolume: .*: \d+ / +(?P<volume>\d+%)")
+re_muted = re.compile(r"\tmuted: (?P<muted>no|yes)")
+re_name = re.compile(r"\tdevice\.description = \"(?P<name>.*)\"")
 
 
 @dataclass
@@ -21,12 +18,18 @@ class Monitor(CallbackMonitor):
 
     def get_updates(self):
 
-        match = re_sound.search(run_cmd("pacmd list-sinks"))
-        if match is None:
-            content = "?"
-        else:
-            sign = "=" if match["muted"] == "no" else "#"
-            name = match["name"]
-            name = self.aliases.get(name, name)
-            content = f"{name}{sign}{match['volume']}"
+        sinks = run_cmd("pacmd list-sinks")
+
+        try:
+            sink = re_default_sink.search(sinks)["default_sink"]
+            volume = re_volume.search(sink)["volume"]
+            muted = re_muted.search(sink)["muted"] == "yes"
+            name = re_name.search(sink)["name"]
+        except TypeError:
+            return State.from_one(self.name, "?")
+
+        sign = "#" if muted else "="
+        name = self.aliases.get(name, name)
+        content = f"{name}{sign}{volume}"
+
         return State.from_one(self.name, content)
