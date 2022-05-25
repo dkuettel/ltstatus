15c3dae624cc537aeecb8c061f8a71f7f18afc5c
Daniel Kuettel <dkuettel@gmail.com>
2022-05-25 23:15:26 +0200

    port process_alerts monitor

--- python/ltstatus/new_monitors/__init__.py
+++ python/ltstatus/new_monitors/__init__.py
@@ -2,3 +2,4 @@ from ._bluetooth import Monitor as bluetooth
 from ._cpu import Monitor as cpu
 from ._datetime import Monitor as datetime
 from ._diskspace_alerts import Monitor as diskspace_alerts
+from ._process_alerts import Monitor as process_alerts
---
+++ python/ltstatus/new_monitors/_process_alerts.py
@@ -0,0 +1,39 @@
+import re
+from dataclasses import dataclass
+from typing import Dict, Iterator, Optional, Union
+
+from psutil import process_iter
+
+from ..alternative import PollingMonitor
+from ..tools import ffield
+
+
+@dataclass
+class Monitor(PollingMonitor):
+    """notify when certain process are running, like, eg, 'steam' that might continue to run in the background"""
+
+    name: str = "process-alerts"
+    flags: Dict[str, Union[str, re.Pattern]] = ffield(dict)
+
+    def updates(self) -> Iterator[Optional[str]]:
+        while True:
+            yield self.get_state()
+
+    def get_state(self) -> Optional[str]:
+
+        raised = set()
+
+        # TODO attrs=["name", "exe", "cmdline"] are useful process attributes here
+        for process in process_iter(attrs=["name"]):
+            for flag, pattern in self.flags.items():
+                if type(pattern) is str:
+                    pattern = re.compile(pattern)
+                    self.flags[flag] = pattern
+                assert type(pattern) == re.Pattern, pattern
+                if any(pattern.fullmatch(v) for v in process.info.values()):
+                    raised.add(flag)
+
+        if not raised:
+            return None
+
+        return ",".join(sorted(raised))
