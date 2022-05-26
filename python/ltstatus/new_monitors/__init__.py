e3f761585258f764420eb1c7cbb06eaea30ebf4f
Daniel Kuettel <dkuettel@gmail.com>
2022-05-26 18:40:43 +0200

    port sound monitor

--- python/ltstatus/new_monitors/__init__.py
+++ python/ltstatus/new_monitors/__init__.py
@@ -4,3 +4,4 @@ from ._datetime import Monitor as datetime
 from ._diskspace_alerts import Monitor as diskspace_alerts
 from ._process_alerts import Monitor as process_alerts
 from ._redshift import Monitor as redshift
+from ._sound import Monitor as sound
---
+++ python/ltstatus/new_monitors/_sound.py
@@ -0,0 +1,70 @@
+import re
+from dataclasses import dataclass
+from typing import Dict
+
+from ..alternative import RealtimeContext, RealtimeMonitor
+from ..tools import NewTailCommand, StopBySigInt, ffield, run_cmd
+
+re_sink_event = re.compile(r"Event '.+' on sink #\d+")
+
+re_default_sink = re.compile(r"^Default Sink: (?P<value>.+)$", re.MULTILINE)
+re_name = re.compile(r"^\tName: (?P<value>.+)$", re.MULTILINE)
+re_description = re.compile(r"^\tDescription: (?P<value>.+)$", re.MULTILINE)
+re_mute = re.compile(r"\tMute: (?P<value>.+)$", re.MULTILINE)
+re_volume = re.compile(r"\tVolume: .* (?P<value>\d+)% .*$", re.MULTILINE)
+
+
+@dataclass
+class Monitor(RealtimeMonitor):
+    name: str = "sound"
+    aliases: Dict[str, str] = ffield(dict)
+
+    def run(self, context: RealtimeContext):
+
+        context.send(self.get_state())
+
+        with NewTailCommand(args=["pactl", "subscribe"], stop=StopBySigInt()) as log:
+            while not context.should_exit():
+                if log.returncode() is not None:
+                    context.send("pactl failed")
+                    break
+                events = list(log.get_some_lines(timeout=1))
+                if len(events) == 0:
+                    continue
+                sink_event = any(re_sink_event.fullmatch(e) for e in events)
+                if not sink_event:
+                    continue
+                context.send(self.get_state())
+
+    def get_state(self) -> str:
+        """
+        we use only 'pactl', and not 'pacmd'
+        for people that use PipeWire to replace pulseaudio
+        'pactl' still works, but 'pacmd' doesnt
+        """
+
+        # NOTE we dont react kindly to anything that we cant parse
+
+        default_sink = re_default_sink.search(run_cmd("pactl info"))["value"]
+
+        sinks = run_cmd("pactl list sinks")
+
+        for name in re_name.finditer(sinks):
+            if name["value"] == default_sink:
+                break
+        name, pos = name["value"], name.end()
+
+        description = re_description.search(sinks, pos)
+        description, pos = description["value"], description.end()
+
+        mute = re_mute.search(sinks, pos)
+        mute, pos = mute["value"] == "yes", mute.end()
+
+        volume = re_volume.search(sinks, pos)
+        volume, pos = int(volume["value"]), volume.end()
+
+        sign = "#" if mute else "="
+        description = self.aliases.get(description, description)
+        content = f"{description}{sign}{volume}%"
+
+        return content
