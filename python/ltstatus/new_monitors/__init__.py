efad28c1189e35997505bbdf7955eeb8d0fb3ec8
Daniel Kuettel <dkuettel@gmail.com>
2022-05-26 16:49:23 +0200

    port redshift monitor

--- python/ltstatus/new_monitors/__init__.py
+++ python/ltstatus/new_monitors/__init__.py
@@ -3,3 +3,4 @@ from ._cpu import Monitor as cpu
 from ._datetime import Monitor as datetime
 from ._diskspace_alerts import Monitor as diskspace_alerts
 from ._process_alerts import Monitor as process_alerts
+from ._redshift import Monitor as redshift
