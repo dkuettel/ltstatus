0108e1f6878fabac1f2f649babea3bc7e9008a7e
Daniel Kuettel <dkuettel@gmail.com>
2022-06-06 01:14:17 +0200

    nvidia as PollingMonitor

--- python/ltstatus/monitors/nvidia.py
+++ python/ltstatus/monitors/nvidia.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
+from typing import Iterator
 
 from pynvml import (
     nvmlDeviceGetHandleByIndex,
@@ -8,41 +11,39 @@ from pynvml import (
     nvmlShutdown,
 )
 
-from ltstatus import RealtimeContext, RealtimeMonitor
+from ltstatus import PollingMonitor
 from ltstatus.indicators import RatioIndicator, bin_themes
 
 
 @dataclass
-class Monitor(RealtimeMonitor):
+class Monitor(PollingMonitor):
     name: str = "nvidia"
-    interval: float = 1.0
     total_compute: RatioIndicator = RatioIndicator(bin_themes["LMH"])
     memory: RatioIndicator = RatioIndicator(bin_themes["LMH"])
 
-    # TODO looks like it could be polling as well?
+    def updates(self) -> Iterator[str]:
+        while True:
+            yield from self.connected_updates()
+            yield "gpu??"
 
-    def run(self, context: RealtimeContext):
-        # TODO how to not have unavailable status? test before and not even add it?
-        # TODO try finally to shut down nvml?
+    def connected_updates(self) -> Iterator[str]:
+        try:
+            # https://docs.nvidia.com/deploy/nvml-api/index.html
+            nvmlInit()
+            handle = nvmlDeviceGetHandleByIndex(0)
 
-        # https://docs.nvidia.com/deploy/nvml-api/index.html
-        nvmlInit()
-        handle = nvmlDeviceGetHandleByIndex(0)
+            while True:
+                # compute has .gpu, .memory; in percent
+                # https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g540824faa6cef45500e0d1dc2f50b321
+                compute = nvmlDeviceGetUtilizationRates(handle)
+                ind_total = self.total_compute.format(compute.gpu / 100)
 
-        while not context.should_exit():
+                # memory has .free, .total, .used; in bytes
+                # https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g2dfeb1db82aa1de91aa6edf941c85ca8
+                memory = nvmlDeviceGetMemoryInfo(handle)
+                ind_memory = self.memory.format(memory.used / memory.total)
 
-            # compute has .gpu, .memory; in percent
-            # https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g540824faa6cef45500e0d1dc2f50b321
-            compute = nvmlDeviceGetUtilizationRates(handle)
-            ind_total = self.total_compute.format(compute.gpu / 100)
+                yield f"gpu{ind_total}{ind_memory}"
 
-            # memory has .free, .total, .used; in bytes
-            # https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g2dfeb1db82aa1de91aa6edf941c85ca8
-            memory = nvmlDeviceGetMemoryInfo(handle)
-            ind_memory = self.memory.format(memory.used / memory.total)
-
-            context.send(f"gpu{ind_total}{ind_memory}")
-
-            context.sleep(self.interval)
-
-        nvmlShutdown()
+        finally:
+            nvmlShutdown()
