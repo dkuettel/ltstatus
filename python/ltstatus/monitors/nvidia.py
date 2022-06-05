d8c70ca068f6ed264512a8f411016019847ba089
Daniel Kuettel <dkuettel@gmail.com>
2022-06-06 01:32:54 +0200

    nvidia with icons

--- python/ltstatus/monitors/nvidia.py
+++ python/ltstatus/monitors/nvidia.py
@@ -18,13 +18,23 @@ from ltstatus.indicators import RatioIndicator, bin_themes
 @dataclass
 class Monitor(PollingMonitor):
     name: str = "nvidia"
-    total_compute: RatioIndicator = RatioIndicator(bin_themes["LMH"])
+    compute: RatioIndicator = RatioIndicator(bin_themes["LMH"])
     memory: RatioIndicator = RatioIndicator(bin_themes["LMH"])
+    prefix: str = "gpu"
+    missing: str = "??"
+    icons: bool = False
+
+    def with_icons(self) -> Monitor:
+        self.compute = RatioIndicator(bin_themes["thermometer"])
+        self.memory = RatioIndicator(bin_themes["battery!"])
+        self.prefix = ""
+        self.missing = ""
+        return self
 
     def updates(self) -> Iterator[str]:
         while True:
             yield from self.connected_updates()
-            yield "gpu??"
+            yield f"{self.prefix}{self.missing}"
 
     def connected_updates(self) -> Iterator[str]:
         try:
@@ -36,14 +46,14 @@ class Monitor(PollingMonitor):
                 # compute has .gpu, .memory; in percent
                 # https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g540824faa6cef45500e0d1dc2f50b321
                 compute = nvmlDeviceGetUtilizationRates(handle)
-                ind_total = self.total_compute.format(compute.gpu / 100)
+                ind_compute = self.compute.format(compute.gpu / 100)
 
                 # memory has .free, .total, .used; in bytes
                 # https://docs.nvidia.com/deploy/nvml-api/group__nvmlDeviceQueries.html#group__nvmlDeviceQueries_1g2dfeb1db82aa1de91aa6edf941c85ca8
                 memory = nvmlDeviceGetMemoryInfo(handle)
                 ind_memory = self.memory.format(memory.used / memory.total)
 
-                yield f"gpu{ind_total}{ind_memory}"
+                yield f"{self.prefix}{ind_compute}{ind_memory}"
 
         finally:
             nvmlShutdown()
