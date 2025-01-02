d3fc04046887afcb6bb1a4f8c3fae341b50cd1f7
Daniel Kuettel <dkuettel@gmail.com>
2025-01-02 11:45:55 +0000

    flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "ltstatus";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      packages.x86_64-linux.default = pkgs.writeScriptBin "ltstatus" ''
+        #!${pkgs.zsh}/bin/zsh
+        set -eu -o pipefail
+        ${pkgs.uv}/bin/uv run --project ${self} --isolated --quiet $@
+      '';
+    };
+}
