500e33d3e55fa4952c7e3a9e487b4f810845bf94
Daniel Kuettel <dkuettel@gmail.com>
2025-01-11 14:30:00 +0100

    dev and app

--- flake.nix
+++ flake.nix
@@ -10,10 +10,15 @@
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     {
-      packages.x86_64-linux.default = pkgs.writeScriptBin "ltstatus" ''
+      packages.x86_64-linux.default = pkgs.buildEnv {
+        name = "ltstatus-env";
+        # TODO in theory should set env here so that uv never tries do manage a python version
+        paths = with pkgs; [ uv python313 ];
+      };
+      packages.x86_64-linux.app = pkgs.writeScriptBin "ltstatus" ''
         #!${pkgs.zsh}/bin/zsh
         set -eu -o pipefail
-        ${pkgs.uv}/bin/uv run --project ${self} --isolated --quiet python $@
+        ${pkgs.uv}/bin/uv run --python=${pkgs.python313}/bin/python --no-python-downloads --project ${self} --isolated --quiet python $@
       '';
     };
 }
