b6a4c94d888b2ad37e34ae20a61b2a0c5e0a952b
Daniel Kuettel <dkuettel@gmail.com>
2025-05-31 22:19:32 +0200

    app_all for testing

--- src/ltstatus/main.py
+++ src/ltstatus/main.py
@@ -7,6 +7,18 @@ import typer
 
 import ltstatus.monitor as m
 
+limits_gb = {
+    Path("/var/lib/docker"): 2.0,
+    Path("/"): 10.0,
+    Path("~"): 5.0,
+}
+
+sound_aliases = {
+    "iFi (by AMR) HD USB Audio Pro": "ifi",
+    "apm.zero": "apm",
+    "Dummy Output": "none",
+}
+
 app = typer.Typer(
     no_args_is_help=True,
     pretty_exceptions_enable=False,
@@ -14,14 +26,38 @@ app = typer.Typer(
 )
 
 
+@app.command("all")
+def app_all():
+    with (
+        m.diskspace_alerts(limits_gb) as alerts,
+        m.cpu() as cpu,
+        m.nvidia() as nvidia,
+        m.spotify() as spotify,
+        m.process_alerts(flags={"steam": re.compile(r".*steam.*")}) as alerts,
+        m.redshift() as redshift,
+        m.bluetooth() as bluetooth,
+        m.sound(sound_aliases) as sound,
+        m.datetime() as datetime,
+    ):
+        while True:
+            time.sleep(2)
+            segments = [
+                alerts(),
+                cpu(),
+                nvidia(),
+                spotify(),
+                alerts(),
+                redshift(),
+                bluetooth(),
+                sound(),
+                datetime(),
+            ]
+            segments = [s for s in segments if s != ""]
+            print(" | ".join(segments), flush=True)
+
+
 @app.command("tmux")
 def app_tmux():
-    limits_gb = {
-        Path("/var/lib/docker"): 2.0,
-        Path("/"): 10.0,
-        Path("~"): 5.0,
-    }
-
     with (
         m.diskspace_alerts(limits_gb) as alerts,
         m.cpu() as cpu,
@@ -41,12 +77,6 @@ def app_tmux():
 
 @app.command("dwm")
 def app_dwm(test: bool = False):
-    sound_aliases = {
-        "iFi (by AMR) HD USB Audio Pro": "ifi",
-        "apm.zero": "apm",
-        "Dummy Output": "none",
-    }
-
     with (
         m.spotify() as spotify,
         m.process_alerts(flags={"steam": re.compile(r".*steam.*")}) as alerts,
