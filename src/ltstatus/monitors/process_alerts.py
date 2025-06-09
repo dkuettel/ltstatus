9d30ca03ba231ab77282f26a198bec7c4be435e4
Daniel Kuettel <dkuettel@gmail.com>
2025-06-09 20:38:11 +0200

    process alerts on larger intervals

--- src/ltstatus/monitors/process_alerts.py
+++ src/ltstatus/monitors/process_alerts.py
@@ -5,19 +5,28 @@ from contextlib import contextmanager
 
 from psutil import process_iter
 
+from ltstatus.tools import Intervals
+
 
 @contextmanager
 def monitor(flags: dict[str, re.Pattern]):
-    # TODO maybe dont do that so often?
-    def fn() -> str:
-        raised = set[str]()
+    interval = Intervals.from_seconds(5)
+    raised = set[str]()
 
-        # TODO attrs=["name", "exe", "cmdline"] are useful process attributes here
-        for process in process_iter(attrs=["name"]):
-            for flag, pattern in flags.items():
+    def fn() -> str:
+        nonlocal interval, raised
+
+        if interval():
+            raised = set[str]()
+
+            # TODO attrs=["name", "exe", "cmdline"] are useful process attributes here
+            raised = {
+                flag
+                for process in process_iter(attrs=["name"])
+                for flag, pattern in flags.items()
                 # TODO really check all that?
-                if any(pattern.fullmatch(v) for v in process.info.values()):
-                    raised.add(flag)
+                if any(pattern.fullmatch(v) for v in process.info.values())
+            }
 
         return ",".join(sorted(raised))
 
