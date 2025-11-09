203fa6b921125c4b10cd071b05820992f5603254
Daniel Kuettel <dkuettel@gmail.com>
2025-11-09 13:42:20 +0100

    diskpie fallback to / if no zfs

--- src/ltstatus/monitors/diskpie.py
+++ src/ltstatus/monitors/diskpie.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import subprocess
 from contextlib import contextmanager
+from psutil import disk_usage
 
 from ltstatus.tools import Intervals
 
@@ -16,14 +17,18 @@ def monitor():
         nonlocal interval, pies, state
 
         if interval():
-            # NOTE assumes there is only one pool
-            result = subprocess.run(
-                ["zfs", "list", "-d", "0", "-pH", "-o", "avail,used"],
-                capture_output=True,
-                text=True,
-            )
-            avail, used = map(int, result.stdout.split())
-            ratio = used / (used + avail)
+            try:
+                # NOTE assumes there is only one pool
+                result = subprocess.run(
+                    ["zfs", "list", "-d", "0", "-pH", "-o", "avail,used"],
+                    capture_output=True,
+                    text=True,
+                    check=True,
+                )
+                avail, used = map(int, result.stdout.split())
+                ratio = used / (used + avail)
+            except:
+                ratio = disk_usage("/").percent / 100
             i = round(ratio * 8)
             state = pies[i]
 
